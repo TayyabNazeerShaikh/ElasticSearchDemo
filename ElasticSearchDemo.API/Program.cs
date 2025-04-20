e80e2dceab54abcd9fd995ebfe06392a9f8d23df
TayyabNazeerShaikh <tayyab.nazeer.shaikh@outlook.in>
2025-04-20 18:27:45 +0530

    Worked on project

--- ElasticSearchDemo.API/Program.cs
+++ ElasticSearchDemo.API/Program.cs
@@ -1,3 +1,8 @@
+using ElasticSearchDemo.API.Services;
+using ElasticSearchDemo.Services;
+using Nest;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +11,23 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSingleton<IElasticClient>(sp =>
+{
+    var settings = new ConnectionSettings(new Uri("http://localhost:9200"))
+        .DefaultIndex("products");
+    return new ElasticClient(settings);
+});
+
+// Register ProductService
+builder.Services.AddScoped<IProductService, ProductService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
